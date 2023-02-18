66e820f5ee2e4b3731d2982b35215c0b3b93b2b7
Jeroen Vandevelde <jeroen.vandevelde@kbc.be>
2023-02-18 23:32:37 +0100

    Added examples of when spring test context caching gets reloaded

--- src/test/java/com/jeroen/dataorientedprogramming/ch2web/PersonStubConfig.java
+++ src/test/java/com/jeroen/dataorientedprogramming/ch2web/PersonStubConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PersonClientConfig {
+public class PersonStubConfig {
     @Bean
     public PersonStub personStub() {
         return Mockito.mock(PersonStub.class);
